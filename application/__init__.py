cc6b2ae35bba8884907b9c5b08ded501bbab7317
Stavros Korokithakis <stavros@korokithakis.net>
2011-02-07 18:40:08 +0200

    Work around Jinja bug.

--- application/__init__.py
+++ application/__init__.py
@@ -8,4 +8,7 @@ from flask import Flask
 app = Flask('application')
 app.config.from_object('application.settings')
 
+# Prevent jinja from caching templates to work around a bug.
+app.jinja_env.cache = None
+
 import urls
