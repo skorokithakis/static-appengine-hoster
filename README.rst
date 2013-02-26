4ec6bc7157efa27d4b235ec54c9407161fb1e9d9
Stavros Korokithakis <stavros@korokithakis.net>
2013-02-26 17:41:15 +0000

    Add static file resolution.

--- README.rst
+++ README.rst
@@ -24,9 +24,14 @@ host and add your files there. To reference media under your media directory
 you can use the {{ MEDIA_URL }} variable in your templates, and to get the URL
 for a specific page you can use the url() function.
 
-You will need to add your site's domains and alias in ``settings.py``, and then
+You can optionally add your site's domains and alias in ``settings.py``, and then
 you can define and name the URLs and specify the template they will be rendered
 with.
+
+Alternatively, you can just create folders called `www.yourdomain.com` for each
+site you want to host, and put html templates in that folder. The folder structure
+will be preserved, so, for example, the file `mydomain/test/hello.html` will be
+accessible under `http://mydomain/test/hello/`.
 
 License
 -------
--- application/views.py
+++ application/views.py
@@ -1,20 +1,21 @@
-from flask import render_template, flash, url_for, redirect, request, abort
+from flask import render_template, redirect, request, abort
 from jinja2 import FileSystemLoader
 
 from application import app
 
 import os
 
+
 def reverse_url(site_nickname):
     """Construct the function that returns reverse URLs."""
-    return lambda url_name: app.config["REVERSE_URLS"][site_nickname][url_name]
+    def reverse(url_name):
+        site = app.config["REVERSE_URLS"].get(site_nickname)
+        if site is None:
+            return ""
+        else:
+            return site.get(url_name, "")
+    return reverse
 
-def get_site_nickname():
-    """Get the site's nickname from the domain."""
-    # Get the domain name and site nickname from the config.
-    domain = request.host.split(":")[0]
-    site_nickname = app.config["SITE_CONFIGURATION"].get(domain, None)
-    return site_nickname
 
 def page_handler(path):
     # Normalise the path.
@@ -23,24 +24,49 @@ def page_handler(path):
     elif not path.startswith("/"):
         path = "/" + path
 
-    site_nickname = get_site_nickname()
+    # Get the domain name and site nickname from the config.
+    domain = request.host.split(":")[0]
+    site_nickname = app.config["SITE_CONFIGURATION"].get(domain, None)
     if site_nickname is None:
-        abort(404)
+        site_nickname = domain
+
+    template_directory = os.path.join(app.root_path, "templates/", site_nickname)
 
     try:
         template_name = app.config["URLS"][site_nickname][path][0]
     except KeyError:
-        abort(404)
+        # Normalize the path to remove slashes from beginning and end.
+        filepath = path[1:-1]
+        # Add the path/index part.
+        paths = [os.path.join(filepath + "index")]
+
+        # Add the path/ part if the path isn't the root.
+        if filepath:
+            paths.append(filepath)
+
+        filenames = []
+        # Add .htm and .html to everything.
+        for filename in paths:
+            filenames.append(filename + ".htm")
+            filenames.append(filename + ".html")
+
+        for filename in filenames:
+            if os.path.exists(os.path.join(template_directory, filename)):
+                template_name = filename
+                break
+        else:
+            abort(404)
 
     # Change the template loading directory.
-    template_directory = os.path.join(app.root_path, "templates/", site_nickname)
     app.jinja_loader = FileSystemLoader(template_directory)
     media_url = "/media/%s/" % site_nickname
     return render_template(template_name, **{"MEDIA_URL": media_url, "url": reverse_url(site_nickname), "debug": app.config.get("DEBUG", False)})
 
+
 def index_handler():
     # Flask can't handle the root URL with a path rule.
     return page_handler("/")
+
 
 def warmup():
     return ''
@@ -50,16 +76,17 @@ def warmup():
 # Handle 404 errors
 @app.errorhandler(404)
 def page_not_found(e):
-    site_nickname = get_site_nickname()
+    domain = request.host.split(":")[0]
+    site_nickname = app.config["SITE_CONFIGURATION"].get(domain, None)
     if site_nickname is None:
-        site_nickname = ""
+        site_nickname = domain
     template_directory = os.path.join(app.root_path, "templates/", site_nickname)
     app.jinja_loader = FileSystemLoader(template_directory)
     media_url = "/media/%s/" % site_nickname
     return render_template('404.html', **{"MEDIA_URL": media_url, "url": reverse_url(site_nickname), "debug": app.config.get("DEBUG", False)}), 404
 
+
 # Handle 500 errors
 @app.errorhandler(500)
 def server_error(e):
     return render_template('500.html'), 500
-
